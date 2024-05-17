7ac7d99e7a6c0748216ab523487ed61157c25f49
CosminSrd <cosmin.surdu@icloud.com>
2024-05-17 19:41:53 +0200

    wda

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
-const express = require('express');
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
 const bodyParser = require('body-parser');
 const youtubeDl = require('youtube-dl-exec');
 
-const app = express();
-const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.use(bodyParser.json());
-app.use(express.static('public'));
+app.prepare().then(() => {
+    createServer((req, res) => {
+        const parsedUrl = parse(req.url, true);
+        const { pathname, query } = parsedUrl;
 
-app.post('/download', (req, res) => {
-    const { url, formato } = req.body;
-    const options = ['--format', 'best'];
-    if (formato === 'mp3') {
-        options.push('--extract-audio', '--audio-format', 'mp3');
-    }
-    youtubeDl(url, options)
-        .then(output => {
-            console.log(output);
-            res.json({ status: 'success' });
-        })
-        .catch(error => {
-            console.error(error);
-            res.json({ status: 'error' });
-        });
-});
-
-app.listen(port, () => {
-    console.log(`Servidor escuchando en http://localhost:${port}`);
+        if (pathname === '/download' && req.method === 'POST') {
+            let data = '';
+            req.on('data', chunk => {
+                data += chunk.toString();
+            });
+            req.on('end', () => {
+                const { url, formato } = JSON.parse(data);
+                const options = ['--format', 'best'];
+                if (formato === 'mp3') {
+                    options.push('--extract-audio', '--audio-format', 'mp3');
+                }
+                youtubeDl(url, options)
+                    .then(output => {
+                        console.log(output);
+                        res.setHeader('Content-Type', 'application/json');
+                        res.end(JSON.stringify({ status: 'success' }));
+                    })
+                    .catch(error => {
+                        console.error(error);
+                        res.setHeader('Content-Type', 'application/json');
+                        res.end(JSON.stringify({ status: 'error' }));
+                    });
+            });
+        } else {
+            handle(req, res, parsedUrl);
+        }
+    }).listen(process.env.PORT || 3000, err => {
+        if (err) throw err;
+        console.log(`Servidor escuchando en http://localhost:${process.env.PORT || 3000}`);
+    });
 });
